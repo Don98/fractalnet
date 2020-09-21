518158bf77bf4ab96560d442c408256fef59e6b1
Don98 <chenqd6@mail2.sysu.edu.cn>
2020-09-21 17:29:19 +0800

    annot

--- fractal/dataloader.py
+++ fractal/dataloader.py
@@ -530,20 +530,19 @@ class Augmenter(object):
     def __call__(self, sample, flip_x=0.5):
 
         if np.random.rand() < flip_x:
-            image, annots = sample['img'], sample['annot']['boxes']
+            image, annots = sample['img'], sample['annot']
             image = image[:, ::-1, :]
 
             rows, cols, channels = image.shape
 
-            x1 = annots[:, 0].copy()
-            x2 = annots[:, 2].copy()
+            x1 = annots['boxes'][:, 0].copy()
+            x2 = annots['boxes'][:, 2].copy()
             
             x_tmp = x1.copy()
 
-            annots[:, 0] = cols - x2
-            annots[:, 2] = cols - x_tmp
+            annots['boxes'][:, 0] = cols - x2
+            annots['boxes'][:, 2] = cols - x_tmp
 
-            the_annot['boxes'] = annots
 
             sample = {'img': image, 'annot': the_annot}
 
