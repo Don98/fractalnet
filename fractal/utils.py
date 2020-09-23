16b269703b2a84124d92686bde08336a768ab116
Don98 <chenqd6@mail2.sysu.edu.cn>
2020-09-23 23:02:09 +0800

    FractalNet

--- fractal/utils.py
+++ fractal/utils.py
@@ -31,9 +31,9 @@ class BasicBlock(nn.Module):
         out = self.drop1(out)
         out = self.relu(out)
         out = self.bn1(out)
-        print(self.conv1.weight.data.size())
-        print(self.conv1.bias.data.size())
-        print("*"*50)
+        # print(self.conv1.weight.data.size())
+        # print(self.conv1.bias.data.size())
+        # print("*"*50)
         # print(self.conv1.weight.data)
         # print(self.conv1.bias.data)
         return out
