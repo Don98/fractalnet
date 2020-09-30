d44bd00419e6c3652afbaa99a07b0ec915c0abc7
Don98 <chenqd6@mail2.sysu.edu.cn>
2020-09-30 20:48:23 +0800

    drop-path

--- load_dic.py
+++ load_dic.py
@@ -3,3 +3,12 @@ import pickle as pkl
 with open('weights1.pkl', 'rb') as wp:
     name_weights = pkl.load(wp)
     state_dict = {}
+    print(name_weights['concat_pool3_7_plus'])
+    #print(name_weights["conv2_12"]["weight"].shape)
+    exit()
+    for i in name_weights.keys():
+        print(i)
+        print("-"*50)
+        for j in name_weights[i].keys():
+            print(j)
+        print("="*50)
