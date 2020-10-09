e5284cdcfeda8fa7065478a234a1682254bd3b37
Don98 <chenqd6@mail2.sysu.edu.cn>
2020-10-09 15:40:15 +0800

    final_version

--- back.py
+++ back.py
@@ -13,6 +13,24 @@ import torch.optim as optim
 from fractal import coco_eval
 from fractal import csv_eval
 
+import argparse
+import collections
+
+import numpy as np
+
+import torch
+import torch.optim as optim
+from torchvision import transforms
+
+from fractal import model
+from fractal import fractalnet
+from fractal.dataloader import CocoDataset, CSVDataset, VocDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, \
+    Normalizer
+from torch.utils.data import DataLoader
+
+from fractal import coco_eval
+from fractal import csv_eval
+import os
 #@torchsnooper.snoop()
 class FractalNet(nn.Module):
 
@@ -23,8 +41,7 @@ class FractalNet(nn.Module):
         if self.training:
             self.drop_ratio = 0.3
         super(FractalNet, self).__init__()
-        self.convH_0 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        # self.convH_0.bias.data = torch.zeros((64))
+        self.convH_0 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=True)
         self.drop1 = nn.Dropout(self.drop_ratio)
         self.relu = nn.ReLU(inplace=True)
         self.bn1 = nn.BatchNorm2d(64)
@@ -38,14 +55,10 @@ class FractalNet(nn.Module):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
                 n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
-
-
                 m.weight.data.normal_(0, math.sqrt(2. / n))
             elif isinstance(m, nn.BatchNorm2d):
                 m.weight.data.fill_(1)
                 m.bias.data.zero_()
-
-
 
         self.freeze_bn()
 
@@ -60,7 +73,7 @@ class FractalNet(nn.Module):
                 layer.eval()
 
     def forward(self, inputs):
-        
+
         img_batch = inputs
         x = self.convH_0(img_batch)
         x = self.drop1(x)
@@ -72,33 +85,10 @@ class FractalNet(nn.Module):
         x2 = self.the_block2(x1)
         x3 = self.the_block3(x2)
         x4 = self.the_block4(x3)
-        
-        # print("target ", annotations)
-        # print("target size : ", annotations.shape)
-        # print("x4 size : ", x4.shape)
-        # print("="*50)
         return x4
 
 def main(args=None):
 
-    import argparse
-    import collections
-    
-    import numpy as np
-    
-    import torch
-    import torch.optim as optim
-    from torchvision import transforms
-    
-    from fractal import model
-    from fractal import fractalnet
-    from fractal.dataloader import CocoDataset, CSVDataset, VocDataset, collater, Resizer, AspectRatioBasedSampler, Augmenter, \
-        Normalizer
-    from torch.utils.data import DataLoader
-    
-    from fractal import coco_eval
-    from fractal import csv_eval
-    import os
     os.environ["CUDA_VISIBLE_DEVICES"] = "5"
     
     parser = argparse.ArgumentParser(description='Simple training script for training a cnn3 network.')
@@ -114,25 +104,8 @@ def main(args=None):
     sampler_val = AspectRatioBasedSampler(dataset_val, batch_size=1, drop_last=False)
     dataloader_val = DataLoader(dataset_val, num_workers=3, collate_fn=collater, batch_sampler=sampler_val)
     backbone = FractalNet(80, BasicBlock, BigBlock,True)
-    # images = []
-    # targets = []
-    # for i in range(2):
-        # d = {}
-        # d['boxes'] = torch.tensor(torch.load("boxes"+str(i) + ".pt"),dtype=torch.float).cuda()
-        # d["labels"] = torch.load("labels" + str(i) + ".pt")
-
-        # d["labels"] = d["labels"].reshape((1,d["labels"].shape[0]))[0].cuda()
-        # targets.append(d)
-        # images.append(torch.tensor(torch.load("img_" + str(i) + ".pt"),dtype=torch.float).cuda())
     model = FasterRCNN(backbone,num_classes=80)
     model = model.cuda()
-
-    #model.training = True
-    #optimizer = optim.Adam(model.parameters(), lr=1e-5)
-    #scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, patience=3, verbose=True)
-    #loss_hist = collections.deque(maxlen=500)
-    #output = model(images, targets)
-
     model = model.float()
     model.eval()
     coco_eval.evaluate_coco(dataloader_val, model)
